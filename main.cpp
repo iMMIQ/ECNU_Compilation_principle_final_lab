178cafe3279108820091b455ca794a8824127a84
Yudong An <ayd@immiq.top>
2022-07-31 18:20:31 +0800

    add more lexer

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #import <iostream>
-#import <map>
 #import <regex>
 #import <string>
+#import <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::regex;
 using std::string;
 
 enum class Token { End, ID, Int, IntNum, Real, RealNum, If, Then, Else, Error };
 
-std::map<Token, std::regex> pattern{
-    {Token::ID, std::regex("[a-zA-Z]([a-zA-Z0-9]){0,63}")},
-    {Token::IntNum, std::regex("[0-9]+")}};
+using Pattern = std::pair<Token, regex>;
+
+// 用于正则表达式匹配
+// 注意匹配顺序
+std::vector<Pattern> pattern{
+    {Token::Int, regex("int")},
+    {Token::Real, regex("real")},
+    {Token::If, regex("if")},
+    {Token::Then, regex("then")},
+    {Token::Else, regex("else")},
+    {Token::ID, regex("[a-zA-Z]([a-zA-Z0-9]){0,63}")},
+    {Token::IntNum, regex("[0-9]+")},
+};
 
 // 标识符以字母开头，以字母或数字结尾。 关键字不能是标识符，标识符的最大长度为
 // 64 个字符。
