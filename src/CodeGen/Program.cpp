aeb25371ccc84b71c86225ed2d2f4fb74b93a610
Yudong An <ayd@immiq.top>
2022-08-09 23:14:33 +0800

    修复printf定义

--- src/CodeGen/Program.cpp
+++ src/CodeGen/Program.cpp
@@ -7,9 +7,13 @@ auto Program::code_gen() -> void {
 
   //  compound_stmt->code_gen();
 
+  auto printf_func = llvm::Function::Create(
+      llvm::FunctionType::get(llvm::Type::getInt32Ty(*context),
+                              {llvm::Type::getInt8PtrTy(*context)}, false),
+      llvm::Function::ExternalLinkage, "printf", module.get());
+
   for (const auto &i : named_values) {
     llvm::Value *value = i.second;
-    llvm::Function *printf_func = module->getFunction("printf");
     llvm::Value *format_str =
         llvm::ConstantDataArray::getString(*context, i.first + "： %d\n");
     llvm::Value *args[] = {format_str, value};
