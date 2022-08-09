8fc3a92c8df289af111bfc75badd5e8b26fe13ea
Zhaomeng Zhang <zzm824@qq.com>
2022-08-10 01:55:38 +0800

    添加运算符优先级和布尔及算术表达式判定

--- src/Parser/ParserUtils.cpp
+++ src/Parser/ParserUtils.cpp
@@ -1,3 +1,29 @@
 #include "ParserUtils.h"
 
+std::map<Token, int> ParserUtils::binary_operator_precedence =
+    init_binary_operator_precedence();
+
 Token ParserUtils::get_next_token() { return cur_token = Lexer::get_token(); }
+
+std::map<Token, int> ParserUtils::init_binary_operator_precedence() {
+  std::map<Token, int> ret;
+  ret[Token::Less] = 10;
+  ret[Token::LessEqual] = 10;
+  ret[Token::Greater] = 10;
+  ret[Token::GreaterEqual] = 10;
+  ret[Token::Equal] = 10;
+  ret[Token::Plus] = 20;
+  ret[Token::Minus] = 20;
+  ret[Token::Multiplies] = 40;
+  ret[Token::Divides] = 40;
+  return ret;
+}
+bool ParserUtils::is_bool_binary_operator() {
+  return cur_token == Token::Less || cur_token == Token::LessEqual ||
+         cur_token == Token::Greater || cur_token == Token::GreaterEqual ||
+         cur_token == Token::Equal;
+}
+bool ParserUtils::is_arith_binary_operator() {
+  return cur_token == Token::Plus || cur_token == Token::Minus ||
+         cur_token == Token::Multiplies || cur_token == Token::Divides;
+}
--- src/Parser/ParserUtils.h
+++ src/Parser/ParserUtils.h
@@ -10,7 +10,11 @@
 class ParserUtils {
 public:
   static inline Token cur_token = Token::Error;
+  static std::map<Token, int> binary_operator_precedence;
   static Token get_next_token();
+  static std::map<Token, int> init_binary_operator_precedence();
+  static bool is_bool_binary_operator();
+  static bool is_arith_binary_operator();
 };
 
 #endif // ECNU_COMPILATION_PRINCIPLE_FINAL_LAB_PARSERUTILS_H
