3cf4ca105caa21cb85354535d6f043af644b6c66
Yudong An <ayd@immiq.top>
2022-08-13 14:26:46 +0800

    [fix bug] 生成布尔运算时类型错误

--- src/CodeGen/BoolBinaryExpr.cpp
+++ src/CodeGen/BoolBinaryExpr.cpp
@@ -7,19 +7,45 @@ auto BoolBinaryExpr::code_gen() -> llvm::Value * {
   if (!l || !r) {
     return nullptr;
   }
+  if (l->getType()->getPointerElementType()->isIntegerTy() &&
+      r->getType()->getPointerElementType()->isDoubleTy()) {
+    l = Program::builder->CreateBitCast(
+        l, llvm::Type::getDoubleTy(*Program::context));
+  } else if (r->getType()->getPointerElementType()->isIntegerTy() &&
+             l->getType()->getPointerElementType()->isDoubleTy()) {
+    r = Program::builder->CreateBitCast(
+        r, llvm::Type::getDoubleTy(*Program::context));
+  }
 
-  switch (op) {
-  case Token::Less:
-    return Program::builder->CreateFCmpOLT(l, r);
-  case Token::LessEqual:
-    return Program::builder->CreateFCmpOLE(l, r);
-  case Token::Greater:
-    return Program::builder->CreateFCmpOGT(l, r);
-  case Token::GreaterEqual:
-    return Program::builder->CreateFCmpOGE(l, r);
-  case Token::Equal:
-    return Program::builder->CreateFCmpOEQ(l, r);
-  default:
-    return nullptr;
+  if (l->getType()->getPointerElementType()->isIntegerTy()) {
+    switch (op) {
+    case Token::Less:
+      return Program::builder->CreateICmpSLT(l, r);
+    case Token::LessEqual:
+      return Program::builder->CreateICmpSLE(l, r);
+    case Token::Greater:
+      return Program::builder->CreateICmpSGT(l, r);
+    case Token::GreaterEqual:
+      return Program::builder->CreateICmpSGE(l, r);
+    case Token::Equal:
+      return Program::builder->CreateICmpEQ(l, r);
+    default:
+      return nullptr;
+    }
+  } else {
+    switch (op) {
+    case Token::Less:
+      return Program::builder->CreateFCmpOLT(l, r);
+    case Token::LessEqual:
+      return Program::builder->CreateFCmpOLE(l, r);
+    case Token::Greater:
+      return Program::builder->CreateFCmpOGT(l, r);
+    case Token::GreaterEqual:
+      return Program::builder->CreateFCmpOGE(l, r);
+    case Token::Equal:
+      return Program::builder->CreateFCmpOEQ(l, r);
+    default:
+      return nullptr;
+    }
   }
 }
