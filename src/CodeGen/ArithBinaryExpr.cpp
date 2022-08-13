5d6ca9146bd4a0cbec6b7335f20b2359d5620e23
Yudong An <ayd@immiq.top>
2022-08-13 14:29:10 +0800

    [fix bug] 生成代数运算时类型错误

--- src/CodeGen/ArithBinaryExpr.cpp
+++ src/CodeGen/ArithBinaryExpr.cpp
@@ -7,17 +7,41 @@ auto ArithBinaryExpr::code_gen() -> llvm::Value * {
   if (!l || !r) {
     return nullptr;
   }
+  if (l->getType()->getPointerElementType()->isIntegerTy() &&
+      r->getType()->getPointerElementType()->isDoubleTy()) {
+    l = Program::builder->CreateBitCast(
+        l, llvm::Type::getDoubleTy(*Program::context));
+  } else if (r->getType()->getPointerElementType()->isIntegerTy() &&
+             l->getType()->getPointerElementType()->isDoubleTy()) {
+    r = Program::builder->CreateBitCast(
+        r, llvm::Type::getDoubleTy(*Program::context));
+  }
 
-  switch (op) {
-  case Token::Plus:
-    return Program::builder->CreateAdd(l, r);
-  case Token::Minus:
-    return Program::builder->CreateSub(l, r);
-  case Token::Multiplies:
-    return Program::builder->CreateMul(l, r);
-  case Token::Divides:
-    return Program::builder->CreateSDiv(l, r);
-  default:
-    return nullptr;
+  if (l->getType()->getPointerElementType()->isIntegerTy()) {
+    switch (op) {
+    case Token::Plus:
+      return Program::builder->CreateAdd(l, r);
+    case Token::Minus:
+      return Program::builder->CreateSub(l, r);
+    case Token::Multiplies:
+      return Program::builder->CreateMul(l, r);
+    case Token::Divides:
+      return Program::builder->CreateSDiv(l, r);
+    default:
+      return nullptr;
+    }
+  } else {
+    switch (op) {
+    case Token::Plus:
+      return Program::builder->CreateFAdd(l, r);
+    case Token::Minus:
+      return Program::builder->CreateFSub(l, r);
+    case Token::Multiplies:
+      return Program::builder->CreateFMul(l, r);
+    case Token::Divides:
+      return Program::builder->CreateFDiv(l, r);
+    default:
+      return nullptr;
+    }
   }
 }
