fc5b5419be812f272b9d7812fdaa573e94653d41
Zhaomeng Zhang <zzm824@qq.com>
2022-08-13 13:30:03 +0800

    修复RealDecl

--- src/Parser/RealDecl.cpp
+++ src/Parser/RealDecl.cpp
@@ -9,12 +9,12 @@ std::unique_ptr<RealDecl> RealDecl::parse() {
   if (ParserUtils::cur_token != Token::Assignment) {
     // TODO: handle invalid
   }
-  if (ParserUtils::get_next_token() != Token::IntNum) {
+  if (ParserUtils::get_next_token() != Token::RealNum) {
     // TODO: handle invalid
   }
-  auto int_num = RealNumExpr::parse();
+  auto real_num = RealNumExpr::parse();
   if (ParserUtils::cur_token != Token::End) {
     // TODO: handle invalid
   }
-  return std::make_unique<RealDecl>(std::move(id), std::move(int_num));
+  return std::make_unique<RealDecl>(std::move(id), std::move(real_num));
 }
