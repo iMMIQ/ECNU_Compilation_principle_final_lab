e9fa806420f5b8e2bb614f2f09a9fac032dad758
Zhaomeng Zhang <zzm824@qq.com>
2022-08-14 19:00:46 +0800

    添加WhileStmt的AST

---
+++ src/AST/WhileStmt.h
@@ -0,0 +1,16 @@
+#ifndef ECNU_COMPILATION_PRINCIPLE_FINAL_LAB_WHILESTMT_H
+#define ECNU_COMPILATION_PRINCIPLE_FINAL_LAB_WHILESTMT_H
+
+#include "BoolBinaryExpr.h"
+#include "Stmt.h"
+class WhileStmt : public Stmt {
+  std::unique_ptr<BoolBinaryExpr> bool_expr;
+  std::unique_ptr<Stmt> stmt;
+
+public:
+  WhileStmt(std::unique_ptr<BoolBinaryExpr> bool_expr,
+            std::unique_ptr<Stmt> stmt)
+      : bool_expr(std::move(bool_expr)), stmt(std::move(stmt)) {}
+};
+
+#endif // ECNU_COMPILATION_PRINCIPLE_FINAL_LAB_WHILESTMT_H
